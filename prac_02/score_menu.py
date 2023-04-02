ca83132e5eafe9cc085647e8994028c57f2e6107
Shreya Ranjan <shreya110@icloud.com>
2023-04-02 13:15:43 +0800

    Add code for score_menu.py

---
+++ prac_02/score_menu.py
@@ -0,0 +1,55 @@
+MENU = """
+(G)et a valid score"
+(P)rint result
+(S)how stars
+(Q)uit
+"""
+
+
+def main():
+    """Display menu."""
+    print(MENU)
+    choice = input(">>>").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = validate_score()
+        elif choice == "P":
+            grades = get_score(score)
+            print(grades)
+        elif choice == "S":
+            stars(score)
+        else:
+            print("Invalid Choice")
+        print(MENU)
+        choice = input(">>>").upper()
+    print("Thank you")
+
+
+def validate_score():
+    """Check whether score is valid."""
+    score = int(input("score:"))
+    while score < 0 or score > 100:
+        print("invalid score")
+        score = int(input("score:"))
+    return score
+
+
+def get_score(score):
+    """Determine grade."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    else:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Passable"
+        elif score < 50:
+            return "Bad"
+
+
+def stars(score):
+    """Display star."""
+    print("*" * score)
+
+
+main()
