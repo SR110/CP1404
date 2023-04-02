0c7e13d39247f67fe2468bbb4b4f20b7e2a81dfe
Shreya Ranjan <shreya110@icloud.com>
2023-04-02 12:42:48 +0800

    Modify variable names

--- prac_01/practice_q1_q2.py
+++ prac_01/practice_q1_q2.py
@@ -2,9 +2,9 @@
 print("Electricity bill estimator")
 cost = int(input("Enter cents per kWh: "))
 daily_use = float(input("Enter daily use in kWh: "))
-billing_days_num = int(input("Enter number of billing days: "))
-esmt_bill = cost * daily_use * billing_days_num / 100
-print(f"Estimated bill: ${esmt_bill:.2f}")
+billing_days_number = int(input("Enter number of billing days: "))
+estimated_bill = cost * daily_use * billing_days_number / 100
+print(f"Estimated bill: ${estimated_bill:.2f}")
 
 # Q2
 print("Electricity bill estimator 2.0")
@@ -12,9 +12,9 @@ TARIFF_11 = 0.244618
 TARIFF_31 = 0.136928
 tariff = int(input("Which tariff? 11 or 31: "))
 daily_use = float(input("Enter daily use in kWh: "))
-billing_days_num = int(input("Enter number of billing days: "))
+billing_days_number = int(input("Enter number of billing days: "))
 if tariff == 11:
-    esmt_bill = TARIFF_11 * daily_use * billing_days_num
+    estimated_bill = TARIFF_11 * daily_use * billing_days_number
 elif tariff == 31:
-    esmt_bill = TARIFF_31 * daily_use * billing_days_num
-print(f"Estimated bill: ${esmt_bill:.2f}")
+    estimated_bill = TARIFF_31 * daily_use * billing_days_number
+print(f"Estimated bill: ${estimated_bill:.2f}")
