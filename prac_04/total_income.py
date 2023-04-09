d46dfd917ac9271485399ac10410c5905a8a10af
Shreya Ranjan <shreya110@icloud.com>
2023-04-09 20:03:30 +0800

    Create print_report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,6 +13,8 @@ def main():
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
+def print_report(incomes, number_of_months):
+    """Print income report."""
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
