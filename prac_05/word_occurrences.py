374223f8603ecb76d52fed710fb5e08a7ab122e2
Shreya Ranjan <shreya110@icloud.com>
2023-04-16 14:57:29 +0800

    Refactor variable

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -4,14 +4,14 @@ Estimate: 20 minutes
 Actual:   16 minutes
 """
 
-word_occurrences = {}
+word_to_occurrences = {}
 string = input("Text: ")  # I am Shreya
 words = string.split()  # ["I", "am", "Shreya"]
 words.sort()
 for i in range(len(words)):
-    if words[i] not in word_occurrences:
-        word_occurrences[words[i]] = 1
+    if words[i] not in word_to_occurrences:
+        word_to_occurrences[words[i]] = 1
     else:
-        word_occurrences[words[i]] += 1
-for word in word_occurrences:
-    print(f"{word:<10}: {word_occurrences[word]}", end="\n")
+        word_to_occurrences[words[i]] += 1
+for word in word_to_occurrences:
+    print(f"{word:<10}: {word_to_occurrences[word]}", end="\n")
