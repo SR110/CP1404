3f5202d7892e24942219264444962441e2f1a572
Shreya Ranjan <shreya110@icloud.com>
2023-04-16 19:01:16 +0800

    Change approach used for program

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,31 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
+# state_code = input("Enter short state: ").upper()
+
+
+# "Look Before You Leap" Approach
+# while state_code != "":
+#     if state_code in CODE_TO_NAME:
+#         print(state_code, "is", CODE_TO_NAME[state_code])
+#     else:
+#         print("Invalid short state")
+#     state_code = input("Enter short state: ").upper()
+
+
+# "Easier to Ask Forgiveness than Permission" Approach
+while True:
+    try:
+        state_code = input("Enter short state: ").upper()
+        if not state_code:
+            break
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except KeyError:
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+
+for state_code in CODE_TO_NAME:
+    print(f"{state_code:>3} is {CODE_TO_NAME[state_code]:>7}")
