32bfde9865b7626e957f5724059d5f133df5c5e1
Shreya Ranjan <shreya110@icloud.com>
2023-05-04 22:26:36 +0800

    Add docstrings under methods

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,21 +1,21 @@
-"""Estimated time -
-Actual time -
+"""Estimated time - 20 minutes
+Actual time - 30 minutes
 """
 
 
 class ProgrammingLanguage:
 
     def __init__(self, language, typing, reflection, year):
+        """Initialise the Programming Language."""
         self.language = language
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __str__(self):
+        """Return representation of a Programming Language."""
         return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
-        if self.typing == "Dynamic":
-            return True
-        else:
-            return False
+        """Check whether typing of Programming Language is dynamic or not."""
+        return self.typing == "Dynamic"
