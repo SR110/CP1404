60157fdddf785da00aafb3808aad946cab62348a
Shreya Ranjan <shreya110@icloud.com>
2023-05-14 19:19:34 +0800

    Add docstring

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,14 +4,17 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Build Kivy Application."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Call event handler when 'Greet' pressed."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Call event handler when 'Clear' pressed."""
         self.root.ids.input_label.text = ""
         self.root.ids.output_label.text = "Enter your name"
 
